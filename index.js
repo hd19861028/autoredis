0ea88fb73f390100c67f34e052d8bf953f53ffe1
hd19861028 <282642547@qq.com>
2016-03-11 14:45:52 +0800

    add default config for index.js file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-
-global.config = require("./config");
+if (!global.config) {
+	global.config = require("./config");
+} else {
+	if (!global.config.db || !global.config.cache) {
+		var json = require("./config");
+		global.config.db = json.db;
+		global.config.cache = json.cache;
+	}
+}
 
 exports.cache = require('./lib/cache');
 exports.mysql = require('./lib/mysql');
