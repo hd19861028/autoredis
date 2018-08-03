70801c60e0c281b292f52fc79b148615b8b5f538
hujindi <hujindi@rayku.net>
2018-08-03 10:35:36 +0800

    fix mysql remove pool

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-if (!global.config) {
-	global.config = require("./config");
-} else {
-	if (!global.config.db || !global.config.cache) {
-		var json = require("./config");
-		global.config.db = json.db;
-		global.config.cache = json.cache;
-	}
-}
 
 exports.cache = require('./lib/cache');
 exports.mysql = require('./lib/mysql');
